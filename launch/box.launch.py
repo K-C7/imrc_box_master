a23087b5530e40c4a618b9ea45ef93c00fe180f7
imrcmini3 <gunma.imrc@gmail.com>
2026-02-27 11:06:40 +0900

    一括起動のlaunchを追加

---
+++ launch/box.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    box_master_node = Node(
+        package='imrc_box_master',
+        executable='box_master_ransac',
+        name='box_master',
+        arguments=['--ros-args', '--log-level', 'info'],
+    )
+
+    angle_judge_node = Node(
+        package='ros_angle_judgement',
+        executable='angle_judgement',
+        name='angle_judgement',
+        arguments=['--ros-args', '--log-level', 'info'],
+
+    )
+
+    return LaunchDescription([
+        box_master_node,
+        angle_judge_node
+    ])
+
