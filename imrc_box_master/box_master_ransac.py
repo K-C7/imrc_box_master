d03e73d403742c5bab00c655278ab80cf7ad819c
imrcmini3 <gunma.imrc@gmail.com>
2026-02-27 14:32:22 +0900

    方向指定に対応、横の距離での調整を追加

--- imrc_box_master/box_master_ransac.py
+++ imrc_box_master/box_master_ransac.py
@@ -6,6 +6,7 @@ from rclpy.executors import MultiThreadedExecutor
 
 import time
 import math
+from enum import Enum
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
@@ -14,6 +15,12 @@ from imrc_messages.action import BoxCommand
 from imrc_messages.msg import GeneralCommand
 from imrc_messages.msg import RobotActionProgress
 from imrc_messages.msg import WallInfo
+
+class Direction(Enum):
+    FRONT = 1
+    BACK = 2
+    LEFT = 3
+    RIGHT = 4
 
 
 class BoxMaster(Node):
@@ -39,19 +46,26 @@ class BoxMaster(Node):
             RobotActionProgress, '/robot_progress', self.lift_progress_callback, 10, callback_group=self.cb_group)
         
         self.wall_sub = self.create_subscription(WallInfo, "/wall", self.wall_callback, 10, callback_group=self.cb_group)
+        self.wall_sub = self.create_subscription(WallInfo, "/wall_raw", self.wall_raw_callback, 10, callback_group=self.cb_group)
 
         # -------------------- 各種フラグ・パラメータ --------------------
+        self.DISTANCE_BOX = 0.325
+        self.DISTANCE_WALL_SIDE_SHORT = 1.180
+        self.DISTANCE_WALL_SIDE_LONG = 1.712
+
+        self.source_direction = Direction.BACK
         self.distance = 0.0
         self.alignment_enable = False
-        self.target_dist = 0.305
-        self.kp_align = 0.1
+        self.isMovingForward = False
+        self.target_dist = self.DISTANCE_WALL_SIDE_SHORT
+        self.kp_align = 0.80
         self.max_speed = 0.20
-        self.dead_zone = 0.02
+        self.dead_zone = 0.03
 
         self.rotate_enable = False
-        self.kp_rotate = 0.5
-        self.tolerance = 0.001
-        self.max_angular_vel = 0.2
+        self.kp_rotate = 0.3
+        self.tolerance = 0.010
+        self.max_angular_vel = 0.3
 
         # -------------------- アクションサーバー --------------------
         self.action_server = ActionServer(
@@ -97,35 +111,102 @@ class BoxMaster(Node):
         if not rclpy.ok():
             return
 
+        if(self.target_dist == self.DISTANCE_WALL_SIDE_LONG):
+            return
+
+        distance = 0.00
+        angle = 0.00
+
+        
+        if(self.source_direction == Direction.FRONT):
+            distance = msg.front_distance
+            angle = msg.front_angle
+        elif(self.source_direction == Direction.BACK):
+            distance = msg.back_distance
+            angle = msg.back_angle - 0.040
+        elif(self.source_direction == Direction.LEFT):
+            distance = msg.left_distance
+            angle = msg.left_angle
+        elif(self.source_direction == Direction.RIGHT):
+            distance = msg.right_distance
+            angle = msg.right_angle
+        else:
+            pass
+
         if self.alignment_enable:
-            self.alignment_callback(msg)
+            self.alignment_callback(distance)
         elif self.rotate_enable:
-            self.rotate_callback(msg)
+            self.rotate_callback(angle)
+        elif self.isMovingForward:
+            pass
         else:
             self.stop_robot()
     
-    def rotate_callback(self, msg):
-        error = -msg.back_angle
+    def wall_raw_callback(self, msg): 
+        # ノード終了時は何もしない
+        if not rclpy.ok():
+            return
+
+        if not(self.target_dist == self.DISTANCE_WALL_SIDE_LONG):
+            return
+
+        distance = 0.00
+        angle = 0.00
+
+        
+        if(self.source_direction == Direction.FRONT):
+            distance = msg.front_distance
+            angle = msg.front_angle
+        elif(self.source_direction == Direction.BACK):
+            distance = msg.back_distance
+            angle = msg.back_angle
+        elif(self.source_direction == Direction.LEFT):
+            distance = msg.left_distance
+            angle = msg.left_angle
+        elif(self.source_direction == Direction.RIGHT):
+            distance = msg.right_distance
+            angle = msg.right_angle
+        else:
+            pass
+
+        if self.alignment_enable:
+            self.alignment_callback(distance)
+        elif self.rotate_enable:
+            self.rotate_callback(angle)
+        elif self.isMovingForward:
+            pass
+        else:
+            self.stop_robot()
+    
+    def rotate_callback(self, error):
+        error = -error
         twist = Twist()
         if abs(error) > self.tolerance:
             speed = self.kp_rotate * error
             speed = max(min(speed, self.max_angular_vel), -self.max_angular_vel)
             twist.angular.z = speed
-            self.get_logger().info('Rotating now. {0}'.format(error))
+            self.get_logger().debug('Rotating now. {0}'.format(error))
         else:
             twist.angular.z = 0.0
             self.rotate_enable = False
             self.get_logger().info('Rotation complete!')
         self.cmd_vel_pub.publish(twist)
     
-    def alignment_callback(self, msg):
-        error = msg.back_distance  - self.target_dist
+    def alignment_callback(self, distance):
+        error = distance  - self.target_dist
         twist = Twist()
         if abs(error) > self.dead_zone:
-            self.get_logger().info('Alignment now. {0}'.format(error))
+            self.get_logger().debug('Alignment now. {0}'.format(error))
             speed = self.kp_align * error
             speed = max(min(speed, self.max_speed), -self.max_speed)
-            twist.linear.x = -speed
+            if(self.source_direction == Direction.FRONT):
+                twist.linear.x = speed
+            elif(self.source_direction == Direction.BACK):
+                twist.linear.x = -speed
+            elif(self.source_direction == Direction.LEFT):
+                twist.linear.y = speed
+            elif(self.source_direction == Direction.RIGHT):
+                twist.linear.y = -speed
         else:
             twist.linear.x = 0.0
             self.alignment_enable = False
@@ -135,22 +216,35 @@ class BoxMaster(Node):
     # ------------------------- アクション実行メイン -------------------------
 
     async def box_command_callback(self, goal_handle):
-        self.get_logger().info(f"Action accepted. Target yaw: {goal_handle.request.command}")
         
-        try:
-            # ここでは目標角度を受け取るだけで、実際の回転はrotate_callbackで行う構成と推測
-            # self.target_yaw は wall_callback 側で参照されるべき
-            pass 
-        except ValueError:
+        if not (goal_handle.request.command == "LONG" or goal_handle.request.command == "SHORT"):
+            self.get_logger().error(f"Action aborted because of invalid command. Command: {goal_handle.request.command}")
             goal_handle.abort()
             return BoxCommand.Result()
         
+        self.get_logger().info(f"Action accepted. Command: {goal_handle.request.command}")
+
         # 1. 指定角度への回転
+        self.source_direction = Direction.BACK
+        self.target_dist = self.DISTANCE_BOX
         self.rotate_enable = True
         if not self.wait_for_flag(lambda: self.rotate_enable, False, goal_handle):
             return self.handle_exit(goal_handle)
         
-        # 2. 距離合わせ
+        # 2. 横壁との距離合わせ
+        self.source_direction = Direction.RIGHT
+        if(goal_handle.request.command == "LONG"):
+            self.target_dist = self.DISTANCE_WALL_SIDE_LONG
+        else:
+            self.target_dist = self.DISTANCE_WALL_SIDE_SHORT
+
+        self.alignment_enable = True
+        if not self.wait_for_flag(lambda: self.alignment_enable, False, goal_handle):
+            return self.handle_exit(goal_handle)
+        
+        # 2. ダンボールとの距離合わせ
+        self.source_direction = Direction.BACK
+        self.target_dist = self.DISTANCE_BOX
         self.alignment_enable = True
         if not self.wait_for_flag(lambda: self.alignment_enable, False, goal_handle):
             return self.handle_exit(goal_handle)
@@ -166,17 +260,20 @@ class BoxMaster(Node):
             return self.handle_exit(goal_handle)
 
         # 5. 少し前進
-        self.get_logger().info("Moving backward...")
+        self.get_logger().info("Moving forward...")
         twist = Twist()
-        twist.linear.x = 0.1
+        twist.linear.x = 0.2
         rate = self.create_rate(10)
+        self.isMovingForward = True
         for _ in range(10):
             if not rclpy.ok() or goal_handle.is_cancel_requested:
                 return self.handle_exit(goal_handle)
             self.cmd_vel_pub.publish(twist)
             rate.sleep()
         
+        self.isMovingForward = False
         self.stop_robot()
+        self.get_logger().info("Box action done.")
         
         # 成功終了
         result = BoxCommand.Result()
@@ -188,9 +285,10 @@ class BoxMaster(Node):
     def wait_for_flag(self, get_flag_func, target_value, goal_handle):
         """フラグ待ちループ。ROS終了・キャンセル・目標達成のいずれかで抜ける"""
         while rclpy.ok() and get_flag_func() != target_value:
+            rate = self.create_rate(100)
             if goal_handle.is_cancel_requested:
                 return False
-            time.sleep(0.1)
+            rate.sleep()
         # 目標に達しており、かつROSが正常ならTrue、それ以外（キャンセル等）はFalse
         return rclpy.ok() and not goal_handle.is_cancel_requested
 
--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/imrc_box_master/launch', ['launch/box.launch.py']),
+        ('share/imrc_box_master/launch', ['launch/box_test.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
